ce5db71421fe00381df663e9b6ff0b9d9879d91f
bacha <bachaeshetu@gmail.com>
2026-05-24 17:13:53 +0300

    Add first Python practice files

---
+++ type_casting.py
@@ -0,0 +1,7 @@
+is_student= False
+if is_student:  print('you are a student')
+else: print('you are not student')
+
+#typecasting used to convert one data type to another type 
+# and  type function is used to check the data type of varibale
+# strring ,int(),float(),bool() are used to conver data
