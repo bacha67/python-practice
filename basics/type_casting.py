7fe2f453406fb79f72603dbcfb65c06e141eb572
bacha <bachaeshetu@gmail.com>
2026-05-24 17:22:24 +0300

    type casting

--- basics/type_casting.py
+++ basics/type_casting.py
@@ -5,3 +5,12 @@ else: print('you are not student')
 #typecasting used to convert one data type to another type 
 # and  type function is used to check the data type of varibale
 # strring ,int(),float(),bool() are used to conver data
+
+name ='bach'
+age = 24
+gpa =3.6
+is_student = True
+print(int(gpa)) #converting float to int
+print(float(age)) #converting intiger to float
+print(str(age))  #converting intger to string
+print(bool(age)) #converting intiger to boolean
