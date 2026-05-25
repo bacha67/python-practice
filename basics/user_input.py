3a7b3f14ecae35e6cfaac3bc6e1bf4c327482282
bacha <bachaeshetu@gmail.com>
2026-05-25 22:03:03 +0300

    uodate user file

--- basics/user_input.py
+++ basics/user_input.py
@@ -1,5 +1,9 @@
 #input() is function that allows us to get user input data and returns the entered data as string
-name = input('what is your name?')
-age = int(input('how old are you?'))
-age = age + 1
-print(f'you are {age} years old {name}')
+# name = input('what is your name?')
+# age = int(input('how old are you?'))
+# age = age + 1
+# print(f'you are {age} years old {name}')
+# print('this is new line', end='   ')
+# print('hello') to print without new line we can use end parameter of print function and set it to empty string or any other string
+# w,x,y,z = "apple","mango", "banana", "cheery"
+# print(w,x,y,z) Many Values to Multiple Variables
