81b29d2ef79c6409ddb189f18d491ce3d3c63f56
bacha <bachaeshetu@gmail.com>
2026-05-26 15:21:18 +0300

    leared boolean values

---
+++ basics/day_01/bool.py
@@ -0,0 +1,19 @@
+#in boolean logic, there are only two values: True and False. These values are used to represent the truth or falsity of a statement or condition.
+#and  empety and zero are false, while non-empty and non-zero values are true.
+#In Python, the following values are considered false:
+#- False
+#- None
+#- 0 (zero of any numeric type)
+#- 0.0 (zero float)
+#- 0j (zero complex)
+#examples of boolean values:
+print(bool(0)) # False
+print(bool(1)) # True
+print(bool(-1)) # True
+print(bool("")) # False
+print(bool("Hello")) # True
+print(bool([])) # False
+print(bool([1, 2, 3])) # True
+print(bool(None)) # False
+print(bool(False)) # False
+print(bool(True)) # True    
