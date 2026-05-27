f04c80b80675cf53510452c3ee7b1f54bba5572c
bacha <bachaeshetu@gmail.com>
2026-05-27 16:08:07 +0300

    learned while loop

---
+++ intermediates/whileLoop.py
@@ -0,0 +1,14 @@
+i =0
+# while i<10:
+#     if i ==5:
+#         break
+# print(i)
+#continue statement
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+else: 
+    print('loop ended') #else statements is execute once when while loop is no longer true
