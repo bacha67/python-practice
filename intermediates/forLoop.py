f8cc82ae6ab8d98ad530fcd65dad839f96262e71
bacha <bachaeshetu@gmail.com>
2026-05-27 16:58:55 +0300

    learned for loop

---
+++ intermediates/forLoop.py
@@ -0,0 +1,31 @@
+fruits = ["apple", "banana", "cherry"]
+y ="banana"
+for x in fruits:
+    print(x)
+
+#Looping Through a String
+
+for l in y:
+    print(l)
+
+
+z ="orange"
+for i in z:
+    if i =="a":
+        break
+    print(i)
+#continue and esle statmens
+k = [1,2,3,4,5]
+for j in k:
+    if j==3:
+        continue
+    print(j)
+else: 
+      print("Finally finished!")
+
+#range
+m=[1,2,3,4,5]
+for f in range(3):
+    print(f)
+    
+
