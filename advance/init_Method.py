24748e8840cf006ba69112a24abc431dcb596c2d
bacha <bachaeshetu@gmail.com>
2026-06-05 10:47:44 +0300

    add newlines for better code readability in init_Method.py

--- advance/init_Method.py
+++ advance/init_Method.py
@@ -9,4 +9,19 @@ class Dog:
 d1=Dog("Buddy",3)
 d1.bark()
 
+# Create the Student class
+class Student:
+    def __init__(self, name, grade):
+         self.name=name
+         self.grade=grade
+
+# Create an object
+s1=Student("Anna","A")
+# Print the grade
+print(s1.grade)
+# Change the grade
+s1.grade="B"
+# Print the updated grade
+print(s1.grade)
+
     
